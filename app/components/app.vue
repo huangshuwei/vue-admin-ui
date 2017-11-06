26bda4e0ad8513122355bd840648034db39d3754
黄书伟 <hsw89520@163.com>
2017-11-06 18:11:58 +0800

    update

--- app/components/app.vue
+++ app/components/app.vue
@@ -1,22 +1,32 @@
 <template>
-
-    <div>
-
-        <left-bar></left-bar>
-
-        <router-view></router-view>
-    </div>
+    <el-container>
+        <el-aside width="200px">
+            <!--left bar-->
+            <left-bar></left-bar>
+        </el-aside>
+        <el-container>
+            <el-header>
+                <!--top bar-->
+                <top-bar></top-bar>
+            </el-header>
+            <el-main>
+                <!--container-->
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
 
     import leftBar from './left-bar.vue'
+    import topBar from './top-bar.vue'
 
     export default{
         name:'app',
 
         components:{
-            leftBar
+            leftBar,topBar
         }
 
 
---
+++ app/components/top-bar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="top-bar">
+        <el-row>
+            <el-col :span="1">
+                <div class="top-bar-expand">
+                    折叠吧
+                </div>
+            </el-col>
+            <el-col :span="23">
+                <el-menu
+                        :default-active="activeIndex"
+                        class="el-menu-demo"
+                        mode="horizontal"
+                        @select="handleSelect"
+                        background-color="#fff"
+                        text-color="#666"
+                        active-text-color="#000">
+                    <el-menu-item index="1">处理中心</el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">我的工作台</template>
+                        <el-menu-item index="2-1">选项1</el-menu-item>
+                        <el-menu-item index="2-2">选项2</el-menu-item>
+                        <el-menu-item index="2-3">选项3</el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+                </el-menu>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                activeIndex: '1'
+            };
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    }
+</script>
