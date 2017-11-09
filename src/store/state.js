d7b7b7f27ad164847b79338befd7d84c348cdb5a
黄书伟 <hsw89520@163.com>
2017-11-09 16:56:55 +0800

    update

--- src/store/state.js
+++ src/store/state.js
@@ -1,15 +1,15 @@
 export default {
     // 所有的对象属性必须添加在root节点下（防止和非公共组件的状态属性重复，重复会导致覆盖）
     root: {
-        leftBarExpandWidth:'200px',
-        leftBarFoldWidth:'64px',
-        isExpand:true,
+        leftBarExpandWidth: '200px',
+        leftBarFoldWidth: '64px',
+        isExpand: true,
 
         // 面包屑
-        crumbsInfoList:[
-            {name:'11',path:'11'},
-            {name:'22',path:'22'},
-            ]
+        crumbsInfoList: [
+            {name: '11', path: '11'},
+            {name: '22', path: '22'},
+        ]
     }
 
 }
