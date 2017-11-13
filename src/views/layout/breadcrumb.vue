a328c1ff105ac13c9d4e93abed6b6304142667db
黄书伟 <hsw89520@163.com>
2017-11-13 14:04:13 +0800

    enter

--- src/views/layout/breadcrumb.vue
+++ src/views/layout/breadcrumb.vue
@@ -2,6 +2,7 @@
     <el-breadcrumb class="dsadasd" separator="/">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
         <el-breadcrumb-item v-for="item in crumbsInfoList" :to="{path:item.path}">{{item.name}}</el-breadcrumb-item>
+        
     </el-breadcrumb>
 </template>
 
