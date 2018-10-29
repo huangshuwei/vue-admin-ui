2df398062c76822172f62af8c779c4167f2c467d
huangshuwei <hsw89520@163.com>
2018-10-29 23:54:52 +0800

    update

--- src/views/_layout/index/top-bar.vue
+++ src/views/_layout/index/top-bar.vue
@@ -18,15 +18,14 @@
 					text-color="#666"
 					active-text-color="#000"
 					@select="handleSelect">
-					<el-menu-item index="1">权限管理</el-menu-item>
-					<el-menu-item index="2">考勤管理</el-menu-item>
+					<el-menu-item index="1">员工管理</el-menu-item>
+					<el-menu-item index="2">系统管理</el-menu-item>
 					<!--<el-submenu index="2">
 						<template slot="title">我的工作台</template>
 						<el-menu-item index="2-1">选项1</el-menu-item>
 						<el-menu-item index="2-2">选项2</el-menu-item>
 						<el-menu-item index="2-3">选项3</el-menu-item>
 					</el-submenu>-->
-					<el-menu-item index="3">系统设置</el-menu-item>
 					<el-menu-item index="4"><a
 						href="http://www.google.com"
 						target="_blank">其他模块</a></el-menu-item>
---
+++ src/views/system-management/department/index.vue
@@ -0,0 +1,3 @@
+<template>
+	<div>部门设置</div>
+</template>
