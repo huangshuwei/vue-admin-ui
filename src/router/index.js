dc2da0d48fd08137af4c0021e6e3fbea9ab1c4c2
huangshuwei <hsw@89520@163.com>
2018-10-30 15:51:56 +0800

    创建分支

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,26 @@ const example = r => require.ensure([], () => r(require('../views/example/index.
 // comps
 const intro = r => require.ensure([], () => r(require('../views/intro/index.vue')), 'intro');
 
-/*
-* 权限管理
-* */
 
+/*
+* 系统管理
+*/
+
+// 01 系统设置
+// 部门设置
+const department = r => require.ensure([], () => r(require('../views/system-management/system-setting/department/index.vue')), 'department');
+// 模块设置
+const module = r => require.ensure([], () => r(require('../views/system-management/system-setting/module/index.vue')), 'module');
+// 岗位设置
+const position = r => require.ensure([], () => r(require('../views/system-management/system-setting/position/index.vue')), 'position');
+// 角色设置
+const role = r => require.ensure([], () => r(require('../views/system-management/system-setting/role/index.vue')), 'role');
+
+
+/*
+* 员工管理
+* */
+const employee = r => require.ensure([], () => r(require('../views/system-management/employee/user/index.vue')), 'employee');
 
 
 
@@ -19,8 +35,8 @@ export default new VueRouter({
     linkActiveClass: 'active',
     routes: [
         {path: '', redirect: '/intro', name: '简介'}, // 默认路由
+        /*{path: '/intro', component: intro, name: '首页', meta: {isHomePage: true}},*/
         {path: '/intro', component: intro, name: '首页', meta: {isHomePage: true}},
-        {path: '/example', component: example, name: '示例'}
         /*  {
               path: '*', component: {
               template: '<div>抱歉，没有您要的页面。</div>'
