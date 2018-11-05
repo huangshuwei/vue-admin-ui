8ea76d450513e6fb82126889042993de72b1bd37
huangshuwei <hsw@89520@163.com>
2018-11-05 19:46:39 +0800

    update

--- src/views/_layout/left-menu-layout/index.vue
+++ src/views/_layout/left-menu-layout/index.vue
@@ -4,8 +4,8 @@
             <left-bar
                     class="left-bar-container"/>
         </el-aside>
-        <el-container style="height: 100%;">
-            <el-header height="35px">
+        <el-container>
+            <el-header style="height: 35px;">
                 <div
                         v-if="rootState.showHeaderOperation"
                         class="header-operation">
@@ -16,7 +16,7 @@
                     <history-tabs class="header-operation-history-tabs"/>
                 </div>
             </el-header>
-            <el-main class="main-container">
+            <el-main class="main-container scroll">
                 <keep-alive>
                     <router-view v-if="$route.meta.keepAlive"/>
                 </keep-alive>
@@ -59,7 +59,7 @@
     import leftBar from './left-bar/index'
     import breadcrumb from './breadcrumb'
     import historyTabs from './history-tabs'
-
+    
     export default {
         name: 'LeftBar',
         components: {
@@ -91,7 +91,39 @@
     .main-container {
         overflow-y: scroll;
         height: calc(100vh - 96px);
+        background: #f0f2f5;
+        border-top: 1px solid #eee;
     }
 
+    .header-operation {
+        height: 35px;
+        /*   line-height: 35px;*/
+        background-color: #f5f4f4;
+        /*border-top: solid 1px #e6e6e6;*/
+    }
 
+    .header-operation:before {
+        content: '';
+        display: inline-block;
+        height: 100%;
+        vertical-align: middle;
+        /* border: 1px solid red;*/ /* so we can see what's going on */
+    }
+
+    .header-operation-breadcrumb {
+        vertical-align: middle;
+        display: inline-block;
+        min-width: 230px;
+        padding-left: 10px;
+    }
+
+    .header-operation-history-tabs {
+        vertical-align: middle;
+        display: inline-block;
+    }
+
+    .header-operation-history-tabs .el-tag {
+        margin-left: 10px;
+        cursor: pointer;
+    }
 </style>
